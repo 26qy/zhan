b825c2200eef5b437a55445d839c75d7b361470f
26qy1 <1299683688@qq.com>
2023-01-12 20:36:03 +0800

    second

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,18 +1,18 @@
 <template>
 	<view class="box" @click="closepop(id)" style="width: 100vw;height: 100vh;">
-		<image class="pic" src="../../static/backg.jpg"style="justify-self:start; position:absolute;width: 100vw;height: 100vh;z-index: -10;" ></image>
+		<image class="pic" src="../../static/分享界面（1）.jpg"style="justify-self:start; position:absolute;width: 100vw;height: 100vh;z-index: -10;" ></image>
 		<view style="display:block; margin: 0 auto;">
 		
 			<view class="box3" style="overflow: hidden;">
-				<view style="text-align:center; position:relative; margin-top: 350rpx; line-height:16vw;">
+				<view style="text-align:center; position:relative; margin-top: 280rpx; line-height:16vw;">
 						
-					<view v-for="(data, key) in dat1" :key='key.id' style="font-size: 50rpx;line-height：16vw;">
+					<view v-for="(data, key) in dat1" :key='key.id' style="font-size: 40rpx;line-height：16vw;margin-left: 10rpx;">
 					{{data[0]}} {{data[1]}} </view>
-					<view v-for="(data, key) in dat2" :key='key.id' style="font-size: 50rpx;line-height：16vw;">
+					<view v-for="(data, key) in dat2" :key='key.id' style="font-size: 40rpx;line-height：16vw;margin-left: 10rpx;">
 					{{data[0]}} {{data[1]}}</view>
-					<view v-for="(data, key) in dat3" :key='key.id' style="font-size: 50rpx;line-height：16vw;">
+					<view v-for="(data, key) in dat3" :key='key.id' style="font-size: 40rpx;line-height：16vw;margin-left: 10rpx;">
 					{{data[0]}} {{data[1]}} </view>
-					<view v-for="(data, key) in dat4" :key='key.id' style="font-size: 50rpx;line-height：16vw;">
+					<view v-for="(data, key) in dat4" :key='key.id' style="font-size: 40rpx;line-height：16vw;margin-left: 10rpx;">
 					{{data[0]}} {{data[1]}}</view>
 					
 				</view>
@@ -20,14 +20,14 @@
 		
 			
 			<view class="box4">
-					<view v-for="(data, key) in dat5" :key='key.id' style="display: inline-table; font-size: 60rpx;font-weight: bold;position: relative;margin-top: 10rpx;margin-left: 390rpx;">
+					<view v-for="(data, key) in dat5" :key='key.id' style="display: inline-table; font-size: 50rpx;font-weight: bold;position: relative;margin-top: 10rpx;margin-left: 250rpx;">
 					{{data[0]}}
 					 </view>
 			</view>
 			
 
 			<view class="box5">	
-				<view class="xun" style="display: flex;justify-content: space-around;width:70vw;position: relative;margin-top: 10rpx;margin-left: 120rpx;">
+				<view class="xun" style="display: flex;justify-content: space-around;width:58vw;position: relative;margin-top: 10rpx;margin-left: 120rpx;">
 				<view v-show="xuhua">
 					<view class="cover" ></view>
 				</view>
@@ -35,7 +35,7 @@
 					
 						<view class="xun1" @tap="on" style="flex: 1;z-index:500" >
 						<uni-transition :show="autoshow" :ref="ani_id0">
-							<image style="width: 23vw;height: 23vw;border-radius: 100rpx;" src="../../static/xun.jpg" mode="aspectFill" @click="popout(0)"></image>
+							<image style="width: 20vw;height: 20vw;border-radius: 100rpx;" :src="msg1>2500?url1:url2" mode="aspectFill" @click="popout(0)"></image>
 					    </uni-transition>
 						</view>
 							
@@ -43,7 +43,7 @@
 					
 					    <view class="xun2" @tap="on" style="flex: 1;z-index:500">
 						<uni-transition :show="autoshow" :ref="ani_id1">
-							<image style="width: 23vw;height: 23vw;border-radius: 100rpx;" src="../../static/xun.jpg" mode="aspectFill" @click="popout(1)"></image>
+							<image style="width: 20vw;height: 20vw;border-radius: 100rpx;" :src="msg2<1200?url3:url4" mode="aspectFill" @click="popout(1)"></image>
 					    </uni-transition>
 						</view>
 				
@@ -51,7 +51,7 @@
 				
 						<view class="xun3"@tap="on" style="flex: 1;z-index:500">
 						<uni-transition :show="autoshow" :ref="ani_id2">
-							<image style="width: 23vw;height: 23vw;border-radius: 100rpx;" src="../../static/xun.jpg" mode="aspectFill" @click="popout(2)"></image>
+							<image style="width: 20vw;height: 20vw;border-radius: 100rpx;" :src="msg3>700?url5:url6" mode="aspectFill" @click="popout(2)"></image>
 					    </uni-transition>
 						</view>
 					
@@ -60,8 +60,8 @@
 			</view>
 		</view>
 		
-	<view class="share" style="overflow: hidden;margin-left: 520rpx;">
-			<image class="share1" src="../../static/pic.jpg" style="width:28vw;height: 28vw;position: relative;" ></image>
+	<view class="share" style="overflow: hidden;margin-left: 480rpx;margin-top: 110rpx;">
+			<image class="share1" src="../../static/pic.jpg" style="width:22vw;height: 22vw;position: relative;" ></image>
 		</view> 
 	
 	<view>
@@ -77,11 +77,20 @@
 		data() {
 			return {
 				title: 'Hello',
-				dat1 : [['食堂消费:',"2300"]],
-				dat2 : [['用水消费:',"1000"]],
-				dat3 : [['游泳消费:',"1000"]],
-				dat4 : [['天猫消费:',"100"]],
+				dat1 : [['食堂消费:',2300]],
+				dat2 : [['用水消费:',1000]],
+				dat3 : [['游泳消费:',400]],
+				dat4 : [['天猫消费:',3200]],
 				dat5 : [["9100"]],
+				msg1 : "",
+				msg2 : "",
+				msg3 : "",
+				url1:require("../../static/3.jpg"),
+				url2:require("../../static/4.jpg"),
+				url3:require("../../static/5.jpg"),
+				url4:require("../../static/6.jpg"),
+				url5:require("../../static/1.jpg"),
+				url6:require("../../static/2.jpg"),
 				show: false,
 				autoshow : true,
 				hadxun : 0,
@@ -127,6 +136,11 @@
 			
 		},
 		methods: {
+			xun(){
+				var msg1 = dat1[1];
+				var msg2 = dat2[1];
+				var msg3 = dat3[1];
+			},
 			
 			popout(id) {
 				if(!this.hadxun){
